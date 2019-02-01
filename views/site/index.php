9418fe008df9be7e8ab0d4d7898d5547515d6fe0
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-02-01 10:36:59 +0000

    Main layout

--- views/site/index.php
+++ views/site/index.php
@@ -4,50 +4,42 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<div class="container content">
+    <div class="row-fluid home">
+        <div class="span4">
+            <h1 class="hidden">Home</h1>
+            <h1 id="greeting"class="blue">
+            <?php
+            $time = date("G");
+            if ($time < 12) {
+            echo 'Good morning!';
+            }
+            else if ($time >= 12 && $time <= 17) {
+            echo 'Good afternoon!';
+            }
+            else if ($time > 17) {
+            echo 'Good evening!';
+            }
+            ?>
+            </h1>
+            <p>At D-formed, we believe in the power of creative communication.</p>
+            <p>We are a small team with skills across all forms of digital media, so your website will be based on great design, efficient code, and expert knowledge. And you will always know who you’re dealing with.</p> 
+            <p>Whether you are an established business looking for an effective online presence, or you’re just setting up on your own, we can produce beautiful, efficient media and strategies to help you reach your audience, create customers and make it happen.</p>
+            <p>So give us a <a href="/contact">call</a>.</p>
         </div>
-
+        <div class="span8">
+            <iframe src="http://player.vimeo.com/video/42827732?color=238149"></iframe>
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="span4">
+            <?php require __DIR__ . '/../boxes/aboutus.php'; ?>
+        </div>
+        <div class="span4">
+            <?php require __DIR__ . '/../boxes/services.php'; ?>
+        </div>
+        <div class="span4">
+            <?php require __DIR__ . '/../boxes/testimonials.php'; ?>
+        </div>
     </div>
 </div>
