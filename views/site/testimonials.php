b467fb273316bcce0108af44328b8cf7104d4231
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-02-01 10:37:10 +0000

    Testimonials

---
+++ views/site/testimonials.php
@@ -0,0 +1,29 @@
+    <blockquote>
+        <p>D-formed hosting provided me with outstanding professional results with a personal service! </p>
+        <cite>&ndash; <a href="http://blankshot.org" title="Blankshot.org" target="_blank">Blankshot.org</a></cite>
+    </blockquote>
+
+    <blockquote>
+        <p>Simple to use and very friendly service! Highly recommended.</p>
+        <cite>&ndash; <a href="http://commonacademy.co.uk" title="Commonacademy.co.uk" target="_blank">Commonacademy.co.uk</a></cite>
+    </blockquote>
+
+    <blockquote>
+        <p>We decided to use D-Formed to manage and host our gaming website and couldn't be happier! We can't recommend them highly enough!</p>
+        <cite>&ndash; <a href="http://gbkclan.co.uk/" title="Gbkclan.co.uk" target="_blank">gbkclan.co.uk</a></cite>
+    </blockquote>
+
+    <blockquote>
+        <p>Dear D-formed, your tech advice and cheerful problem-solving have made a big difference.</p>
+        <cite>&ndash; <a href="http://katyevansbush.com" title="Katyevansbush.com" target="_blank">KatyEvansBush.com</a></cite>
+    </blockquote>
+    
+    <blockquote>
+        <p>The guys at D-Formed have been great in helping me develop my site idea and host it. Best of all the services I've used over the last eight years.</p>
+        <cite>&ndash; <a href="http://boredofbrighton.co.uk/" title="boredofbrighton.co.uk" target="_blank">boredofbrighton.co.uk</a></cite>
+    </blockquote>
+    
+    <blockquote>
+        <p>The D-Formed team are always helpful and efficient and we have been very impressed with all the work they have done for us.</p>
+        <cite>&ndash; <a href="http://la.d-formed.net/" title="Locum Assistant" target="_blank">Locum Assistant</a></cite>
+    </blockquote>
