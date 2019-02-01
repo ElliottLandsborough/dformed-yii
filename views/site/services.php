9a0cbc6304689c3e144d3779f90682cdf05d20a0
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-02-01 11:33:57 +0000

    Services

---
+++ views/site/services.php
@@ -0,0 +1,136 @@
+<div class="container content">
+    <div class="row-fluid">
+        <div class="span8 services">
+            <div class="serviceswrap">
+                <h1 class="blue services">Our Services</h1>
+                <div id="services">
+                    <div class="row-fluid">
+                        <div class="span4">
+                            <div id="services-list">
+                                <ul>
+                                    <li><a id="consultation" class="switcher"><h2>Consultation</h2></a></li>
+                                    <li><a id="design" class="switcher"><h2>Design</h2></a></li>
+                                    <li><a id="development" class="switcher"><h2>Development</h2></a></li>
+                                    <li><a id="branding" class="switcher"><h2>Branding</h2></a></li>
+                                    <li><a id="ecommerce" class="switcher"><h2>eCommerce</h2></a></li>
+                                    <li><a id="cms" class="switcher"><h2>CMS Development</h2></a></li>
+                                    <li><a id="social" class="switcher"><h2>Social Integration</h2></a></li>
+                                    <li><a id="seo" class="switcher"><h2>SEO</h2></a></li>
+                                    <li><a id="hosting" class="switcher"><h2>Web Hosting</h2></a></li>
+                                    <li><a id="associates" class="switcher"><h2>Associates</h2></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="span8">
+                            <div id="switcher-panel"></div>
+                            <div id="consultation-content" class="switcher-content show">
+                                <h2>Consultation</h2>
+                                <p>We don’t do off-the-peg. Every website we make is tailored to the specific needs of a client, with future growth in mind. If you come to us we will listen to you, look at what you do, make our recommendations, and turn your ideas into solid specifications.</p>
+                                <p>We work closely with our clients throughout the design process, so that everything is to their complete satisfaction. We want you to love your new website as much as we do.</p>
+                            </div>
+                            <div id="design-content" class="switcher-content">
+                                <h2>Design</h2>
+                                <h4>Creativity</h4>
+                                <p>We like to see things differently at D-Formed and we believe that with a little imagination we can turn good designs into great ones. We aren’t just rehashing other people’s ideas – we will find creative solutions for your needs and deliver a website that looks, feels, and is awesome.</p>
+                                <h4>Usability</h4>
+                                <p>Usability is at the heart of good design. We make not only beautiful, but effective and intuitive user interfaces.</p>
+                                <h4>Print media</h4>
+                                <p>We specialise in design for websites and digital applications, but we can create new designs for a number of other uses:</p>
+                                <ul>
+                                <li>advertisements</li>
+                                <li>banners</li>
+                                <li>flyers</li>
+                                <li>posters</li>
+                                <li>other printed publications</li>
+                                </ul>
+                                <p>In addition to this, we can develop your logo and visual identity, as part of developing your website – so your business will have an integrated public presence. See our Branding section (left).</p>
+                                <p>If you are interested in hiring us for graphic design work, get in touch with <a href="/contact">Greg</a>.</p>
+                            </div>
+                            <div id="development-content" class="switcher-content">
+                                <h2>Development</h2>
+                                <p>We can create technical specifications for, design, and then develop web applications for any purpose.</p>
+                                <p>That is, we can make your website work, no matter what you need it to do. Basically, we do coding for fun.</p>
+                                <p>This is where we could go on and on all about the technical background to what we can offer you – but if you understood all the jargon, you’d probably know how to do it yourself. We recognize that you’re coming to us so we can know all this stuff and you can relax in the knowledge that you’re getting an awesome website.</p>
+                                <p>Just to show what we do know our stuff, here is some of it:</p>
+                                <h4>PHP</h4>
+                                <ul>
+                                    <li>Custom CMS software</li>
+                                    <li>File upload/manipulation scripts</li>
+                                    <li>RSS/XML parsing</li>
+                                    <li>SQL database interaction</li>
+                                    <li>User management software</li>
+                                    <li>Regular expression-based data manipulation</li>
+                                </ul>
+                                <h4>JavaScript</h4>
+                                <ul>
+                                    <li>jQuery/MooTools libraries</li>
+                                    <li>DOM manipulation</li>
+                                    <li>AJAX and JSON functionality</li>
+                                    <li>Object-Oriented JavaScript</li>
+                                    <li>Animation effects</li>
+                                    <li>Social media buttons</li>
+                                </ul>
+                                <h4>HTML5 and CSS3</h4>
+                                <p>We are committed to using only valid XHTML and CSS – that is, we adhere to the highest standards in coding practice so your site will be as future-proof as we can make it. We also utilise the advanced features of HTML5 and CSS3. Coding awesomeness.</p>
+                                <h4>Third-party software</h4>
+                                <ul>
+                                    <li>Facebook API applications</li>
+                                    <li>Wordpress themes/plugins</li>
+                                    <li>Drupal/Joomla CMS software</li>
+                                    <li>AWStats/Google analytic software</li>
+                                </ul>
+                                <p>e mainly use PHP and JavaScript, but we can also build on different frameworks, according to what you need. We do this because we love it, so if you have a question, please ask! We’ll be happy to talk you through it.</p>
+                            </div>
+                            <div id="branding-content" class="switcher-content">
+                                <h2>Branding</h2>
+                                <p>Who are you? What does the look of your website and logo say about your business, even before anyone speaks to you?</p>
+                                <p>We can help you to develop a visual identity for your business that will really express your core values. You’ll need a logo and a general look that will carry you through, and we can design your business cards and stationery as well as your website. </p>
+                            </div>
+                            <div id="ecommerce-content" class="switcher-content">
+                                <h2>eCommerce</h2>
+                                <p>We can develop online shops of all shapes and sizes. If you have something to sell, we’ll help make it easy for people to buy it.</p>
+                                <p>Our experience in eCommerce ranges a number of different sopping cart platforms, such as:</p>
+                                <ul>
+                                    <li>Wordpress eCommerce</li>
+                                    <li>PrestaShop</li>
+                                    <li>Shopify</li>
+                                </ul>
+                            </div>
+                            <div id="cms-content" class="switcher-content">
+                                <h2>CMS Development</h2>
+                                <p>We can offer you various solutions for managing content on your site. It’s important to get the right system to keep your website as up-to-date as it needs to be. </p>
+                                <p>We can build bespoke content management systems customised to your needs, and we can also work with open-source and others cost-effective systems. CMS platforms we’ve worked with include Wordpress, Drupal, Joomla, and others. </p>
+                                <p>Just ask us. </p>
+                            </div>
+                            <div id="social-content" class="switcher-content">
+                                <h2>Social Integration</h2>
+                                <p>We can work with a variety of APIs to integrate your social media accounts into your website – and vice versa. </p>
+                                <p>For instance, we’ve created instant Instagram and Flickr website galleries, Twitter feed aggregators, and more.</p>
+                                <p>We can also create a social media marketing strategy for you, if you want to integrate a blog page on your site into your wider communications.</p>
+                            </div>
+                            <div id="seo-content" class="switcher-content">
+                                <h2>SEO Content</h2>
+                                <p>All of our sites are developed with search engine optimisation in mind. We can optimise your site for your chosen key words. If you haven’t identified your key words, we can help you work out the best ones. </p>
+                                <p>Through our associates, we can help you develop your SEO strategy and generate detailed SEO reports.</p>
+                            </div>
+                            <div id="hosting-content" class="switcher-content">
+                                <h2>Web Hosting</h2>
+                                <p>In addition to design and development, we can offer hosting for your website. </p>
+                                <p>Our server is part of a world class datacentre is located in Roubaix, France. It is directly connected to major European exchanges like the AMS-IX and LINX via our fully redundant multi-homed network. Our datacentre is monitored 24/7 by highly skilled technicians and this means we can provide a 99.9% uptime guarantee for all our hosting customers. This service is only available to D-Formed customers. </p>
+                            </div>
+                            <div id="associates-content" class="switcher-content">
+                                <h2>Associates</h2>
+                                <p>We have a network of professional associates who can provide services in addition to what we an deliver in the team. Let us know if you need copywriting, in-depth SEO, or photography for your project. </p>
+                                <p>All our associates are listed in our <a href="/contact">contact</a> page.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="span4 aboutright">
+            <?php require __DIR__ . '/../boxes/biggear.php'; ?>
+            <?php require __DIR__ . '/../boxes/testimonials.php'; ?>
+        </div>
+    </div>
+</div>
